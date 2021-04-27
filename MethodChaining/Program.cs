2186234586aef1dd983a7e26cc2dbee3403604ac
Yasmin-AlGhamdi <82459882+Yasmin-AlGhamdi@users.noreply.github.com>
2021-04-27 14:42:27 +0300

    Add files via upload

---
+++ MethodChaining/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MethodChaining
+{
+    class EasyMath
+    {
+        private int firstValue;
+        private int secondValue;
+
+        public EasyMath setFirstOperand(int value)
+        {
+            this.firstValue = value;
+            return this;
+        }
+        public EasyMath setSecondOperand(int value)
+        {
+            this.secondValue = value;
+            return this;
+        }
+        public int sum()
+        {
+            return this.firstValue + this.secondValue;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EasyMath em = new EasyMath();
+            int result = em.setFirstOperand(50).setSecondOperand(30).sum();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        
+        }
+    }
+}
